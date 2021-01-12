491eebbd0afd7327532e88d6cfa9ce76f13b13b2
yusupsans <adithyayusup16@gmail.com>
2021-01-11 19:41:15 -0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 <body>
 <h1>Hello world</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptates voluptas molestias labore, vitae reprehenderit, cumque distinctio hic ratione ipsa laboriosam reiciendis aut illum tempora error excepturi voluptatibus explicabo possimus.</p>
-    
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
