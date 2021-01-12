a9b194908e1966e8e63b0e81b270c486f101312d
yusupsans <adithyayusup16@gmail.com>
2021-01-11 19:32:13 -0800

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>git remote</title>
+</head>
+<body>
+<h1>Hello world</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, voluptates voluptas molestias labore, vitae reprehenderit, cumque distinctio hic ratione ipsa laboriosam reiciendis aut illum tempora error excepturi voluptatibus explicabo possimus.</p>
+    
+</body>
+</html>
